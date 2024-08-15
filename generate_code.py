e009478e6ca02073a632cf45ec142426bc59be11
Rohh Thone <bukreevda@transport.mos.ru>
2024-08-15 19:16:41 +0300

    'require'

---
+++ generate_code.py
@@ -0,0 +1,56 @@
+from openai import OpenAI
+import os
+
+CURRENTDIR = os.getcwd()
+INPUTFILE = os.path.join(CURRENTDIR, 'input', 'prompt.txt')
+OUTPUTDIR = os.path.join(CURRENTDIR, 'output')
+OUTPUTFILE = os.path.join(OUTPUTDIR, 'response')
+
+# Set the API key
+KEY = 'YourDoubleAPIKEY_forsafeinjectionprotection__mostlymental'
+KEYSECONDLY = 'YourDoubleAPIKEY_forsafeinjectionprotection__mostlymental'
+
+# Use the second key as the API key for the example
+os.environ["OPENAI_API_KEY"] = KEYSECONDLY
+
+os.makedirs(OUTPUTDIR, exist_ok=True)
+
+def generatin(subcontext):
+  context = f"{subcontext}"
+
+  # gets API Key from environment variable OPENAI_API_KEY
+  client = OpenAI()
+
+  # Non-streaming:
+  print("----- standard request -----")
+  completion = client.chat.completions.create(
+      model="gpt-4",
+      messages=[
+          {
+              "role": "user",
+              "content": context
+          },
+      ],
+  )
+  print(completion.choices[0].message.content)
+  return completion.choices[0].message.content
+
+def init():
+  count = 0
+  
+  # count files in the output directory
+  for file in os.listdir(OUTPUTDIR):
+    if file.endswith('.txt'):
+      count += 1
+
+  with open(INPUTFILE, 'r', encoding='utf-8') as f:
+    contexted = f.read()
+
+
+  data = generatin(contexted)
+
+  with open(OUTPUTFILE + count + '.txt', 'w', encoding='utf-8') as f:
+      f.write(data)
+
+if __name__ == '__main__':
+  init()
